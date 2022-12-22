b2abb3b888ce897848972b3f9dcca556a98ccdb8
Furkan Güler <furkang2k20@outlook.com>
2022-12-22 11:29:48 +0300

    route fixes

--- server/server.js
+++ server/server.js
@@ -24,7 +24,11 @@ app.use(express.json());
 app.use(favicon("favicon.ico"));
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/", (req, res) => {
+app.get("/", (req, res) => {
+  res.send("tune posted");
+});
+
+app.post("/posttune", (req, res) => {
   twit.postTune();
   res.send("tune posted");
 });
